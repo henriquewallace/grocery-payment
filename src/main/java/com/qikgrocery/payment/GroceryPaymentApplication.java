342cae2ad37793ea4ba02848c979f6a585e51a31
henriquewallace <henrique.wallace@hotmail.com>
2024-04-23 23:35:15 -0300

    chore: initial configs

---
+++ src/main/java/com/qikgrocery/payment/GroceryPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.qikgrocery.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qikgrocery/payment/GroceryPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qikgrocery.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
